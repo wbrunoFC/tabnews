a772e5eda342f404029dd72ff0de4f9bee8981e4
Bruno costa <58226621+wbrunoFC@users.noreply.github.com>
2023-12-12 12:23:14 +0000

    adiciona script de lint

--- .next/static/webpack/webpack.c87e7a8c7c06a189.hot-update.js
+++ .next/static/webpack/webpack.c87e7a8c7c06a189.hot-update.js
@@ -7,12 +7,19 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-self["webpackHotUpdate_N_E"]("webpack",{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ /* webpack/runtime/getFullHash */
-/******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "43e2ee2a8d106e76"; }
-/******/ }();
-/******/ 
-/******/ }
+self["webpackHotUpdate_N_E"](
+  "webpack",
+  {},
+  /******/ function (__webpack_require__) {
+    // webpackRuntimeModules
+    /******/ /* webpack/runtime/getFullHash */
+    /******/ !(function () {
+      /******/ __webpack_require__.h = function () {
+        return "43e2ee2a8d106e76";
+      };
+      /******/
+    })();
+    /******/
+    /******/
+  },
 );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1> opa </h1>
+function Home() {
+  return <h1> opa </h1>;
 }
 
 export default Home;
